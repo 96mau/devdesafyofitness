5242ff07ccfde1d3d373cacc2d537571202af9ed
96mau <mauricioperezgarcia777@gmail.com>
2023-08-14 07:41:47 -0600

    ArribaClientesyPesos

---
+++ app/Http/Livewire/Graficaspesos.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Graficaspesos extends Component
+{
+    public function render()
+    {
+        return view('livewire.graficaspesos');
+    }
+}
